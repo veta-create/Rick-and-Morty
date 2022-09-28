2f9508f0111a567aeffc5b3cce6efaade2b2a8ae
@veta-create <vita.eksuzyan@bk.ru>
2022-09-28 16:30:03 +0300

    new grid

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import React from 'react'
-import './App.css'
-import rick from './assets/images/rick.png'
-import CardsContent from './components/CardsContent/CardsContent'
-import Header from './components/Header/Header'
-import Pagination from './components/Pagination/Pagination'
-import Footer from './components/Footer/Footer'
+import React from "react";
+import "./App.css";
+import rick from "./assets/images/rick.png";
+import CardsContent from "./components/CardsContent/CardsContent";
+import Header from "./components/Header/Header";
 
 const App = (props) => {
   return (
-    <div className='app-wrapper'>
-      <Header filterData={props.store.state.filterData}
-       switchActiveFilter={props.store.switchActiveFilter}
-       changeNameText={props.store.changeNameText} />
+    <div className="app-wrapper">
+      <Header
+        filterData={props.store.state.filterData}
+        switchActiveFilter={props.store.switchActiveFilter}
+        changeNameText={props.store.changeNameText}
+      />
       <CardsContent cardsData={props.store.state.cardsData} />
-      <div className='forRick'>
-        <img src={rick} className='rick'></img>
+      <div className="forRick">
+        <img src={rick} className="rick"></img>
       </div>
-      <Pagination />
-      <Footer />
     </div>
-  )
-}
+  );
+};
 
 export default App;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,22 @@
 import React from "react";
-import s from './Header.module.css'
+import s from "./Header.module.css";
 import Filter from "../Filter/Filter";
+import Pagination from "../Pagination/Pagination";
 
 const Header = (props) => {
   return (
     <div className={s.header}>
       <div className={s.title}>Rick Database</div>
-      <div  className={s.filter}><Filter filterData={props.filterData}
-       switchActiveFilter={props.switchActiveFilter}
-       changeNameText={props.changeNameText} /></div>
-    </div>)
-}
+      <Pagination />
+      <div className={s.filter}>
+        <Filter
+          filterData={props.filterData}
+          switchActiveFilter={props.switchActiveFilter}
+          changeNameText={props.changeNameText}
+        />
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
