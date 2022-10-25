4f33c0f88cdad6b0a799d03022dc077d69901031
@veta-create <vita.eksuzyan@bk.ru>
2022-10-25 13:54:33 +0300

    fixed filters with grid

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,57 +1,63 @@
 import React from "react";
-import { changeNameFilterTextAC, switchActiveFilterAC } from "../../redux/filterReducer";
+import {
+  changeNameFilterTextAC,
+  switchActiveFilterAC,
+} from "../../redux/filterReducer";
 import s from "./Filter.module.css";
 
 const Filter = (props) => {
   const onSwitchingFilters = (event) => {
     let value = event.target.value;
     let id = event.target.id;
-    props.switchingFilters(id, value)
+    props.switchingFilters(id, value);
   };
 
   const onChangeFilterByName = (event) => {
     let newText = event.target.value;
-    props.changeFilterByName(newText)
+    props.changeFilterByName(newText);
   };
 
   return (
     <div className={s.filter}>
       <div className={s.title}>Filter by:</div>
-      <div className={s.byStatus}>
-        status:
-        <select
-          id="byStatus"
-          value={props.filterData.statusFilter.activeFilter}
-          onChange={onSwitchingFilters}
-        >
-          {props.filterData.statusFilter.filters.map((o) => (
-            <option key={o.id} id={o.id} value={o.value}>
-              {o.name}
-            </option>
-          ))}
-        </select>
+      <div className={s.filters}>
+        <div className={s.byStatus}>
+          status:
+          <select
+            id="byStatus"
+            value={props.filterData.statusFilter.activeFilter}
+            onChange={onSwitchingFilters}
+          >
+            {props.filterData.statusFilter.filters.map((o) => (
+              <option key={o.id} id={o.id} value={o.value}>
+                {o.name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className={s.bySpecies}>
+          species:
+          <select
+            id="bySpecies"
+            value={props.filterData.speciesFilter.activeFilter}
+            onChange={onSwitchingFilters}
+          >
+            {props.filterData.speciesFilter.filters.map((o) => (
+              <option key={o.id} id={o.id} value={o.value}>
+                {o.name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className={s.byName}>
+          name:{" "}
+          <input
+            value={props.filterData.nameFilter.name}
+            onChange={onChangeFilterByName}
+          ></input>
+        </div>
       </div>
-      <div className={s.bySpecies}>
-        species:
-        <select
-          id="bySpecies"
-          value={props.filterData.speciesFilter.activeFilter}
-          onChange={onSwitchingFilters}
-        >
-          {props.filterData.speciesFilter.filters.map((o) => (
-            <option key={o.id} id={o.id} value={o.value}>
-              {o.name}
-            </option>
-          ))}
-        </select>
-      </div>
-      <div className={s.byName}>
-        name:{" "}
-        <input
-          value={props.filterData.nameFilter.name}
-          onChange={onChangeFilterByName}
-        ></input>
-      </div>
+
       {/* <select value={props.filterData.activeFilter} onChange={handleSelectChange}>
       </select> */}
     </div>
